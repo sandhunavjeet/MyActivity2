1090c74e6baca6be532acdff93e17c1866752599
nav <navsandhu343@gmail.com>
2019-10-29 21:16:50 -0400

    working on mainactivity

--- app/src/main/java/com/example/myactivity2/MainActivity.java
+++ app/src/main/java/com/example/myactivity2/MainActivity.java
@@ -1,14 +1,145 @@
 package com.example.myactivity2;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
+import io.particle.android.sdk.cloud.ParticleCloud;
+import io.particle.android.sdk.cloud.ParticleCloudSDK;
+import io.particle.android.sdk.cloud.ParticleDevice;
+import io.particle.android.sdk.cloud.exceptions.ParticleCloudException;
+import io.particle.android.sdk.utils.Async;
 public class MainActivity extends AppCompatActivity {
+    private final String TAG="HOME_ASSISTANT";
 
+    // Particle Account Info
+    private final String PARTICLE_USERNAME = "navsandhu343@gmail.com";
+    private final String PARTICLE_PASSWORD = "Ajaib@111";
+
+    // Particle device-specific info
+    private final String DEVICE_ID = "370034001047363333343437";
+
+    // Particle device
+    private ParticleDevice myDevice;
+
+    TextView txtResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the connection to the Particle Cloud
+        ParticleCloudSDK.init(this.getApplicationContext());
+
+        // Setup the device
+        getDeviceFromCloud();
+
+        //get TextView from activity_main.xml
+        this.txtResult =  findViewById(R.id.txtResult);
+    }
+
+    public void getDeviceFromCloud() {
+
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+                particleCloud.logIn(PARTICLE_USERNAME, PARTICLE_PASSWORD);
+                myDevice = particleCloud.getDevice(DEVICE_ID);
+                return -1;
+
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+
+                Log.d(TAG, "Successfully connected device to Cloud");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+
+    public void turnLightsOnOff() {
+
+        Async.executeAsync(ParticleCloudSDK.getCloud(), new Async.ApiWork<ParticleCloud, Object>() {
+            @Override
+            public Object callApi(@NonNull ParticleCloud particleCloud) throws ParticleCloudException, IOException {
+
+                List<String> functionParameters = new ArrayList<String>();
+                functionParameters.add(txtResult.getText().toString());
+                try {
+                    myDevice.callFunction("lightsOnOff", functionParameters);
+
+                } catch (ParticleDevice.FunctionDoesNotExistException e1) {
+                    e1.printStackTrace();
+                }
+
+
+                return -1;
+            }
+
+            @Override
+            public void onSuccess(Object o) {
+                // put your success message here
+                Log.d(TAG, "Lights Turned On!");
+            }
+
+            @Override
+            public void onFailure(ParticleCloudException exception) {
+                Log.d(TAG, exception.getBestMessage());
+            }
+        });
+    }
+
+
+    public void recordButtonClick(View view){
+        showSpeechInput();
+    }
+
+    public void showSpeechInput(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,"Give Command!");
+        try {
+            startActivityForResult(intent, 100);
+        }
+        catch (ActivityNotFoundException e){
+            Toast.makeText(this,"DEVICE DOES NOT SUPPORT SPEECH RECOGNITION",Toast.LENGTH_LONG);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100){
+            if (resultCode == RESULT_OK && data != null){
+
+                ArrayList<String> speechResult = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                this.txtResult.setText(speechResult.get(0).toLowerCase());
+                this.turnLightsOnOff();
+            }
+        }
+
     }
 }
